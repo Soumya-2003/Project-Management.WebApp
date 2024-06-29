9f0719d8c4bb5a9974dbfc502e72087787346f1b
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-06-29 20:05:35 +0530

    Pages Added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import ProfileInfo from "../components/ProfileInfo";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+	return (
+		<div className='m-4'>
+			<Search />
+			<SortRepos />
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				<ProfileInfo />
+				{/* <Repos /> */}
+				{/* <Spinner /> */}
+			</div>
+		</div>
+	);
+};
 
 export default HomePage
