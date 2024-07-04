e07689e6f733dc328ebf7366946eb84b7e8d4590
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-07-04 20:51:30 +0530

    Authentication using PassportJS & Login and Logout functionality added

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import "./passport/github.auth.js";
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/authRoutes.route.js";
 import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongodb.js";
 
+import passport from "passport";
+
+import session from "express-session";
+
 dotenv.config();
 
 const app = express();
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 
 app.get("/", (req, res) => {
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,22 +4,23 @@ import { FaHeart } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
 import { PiSignInBold } from "react-icons/pi";
 import { MdEditDocument } from "react-icons/md";
-
 import Logout from "./Logout";
-// import { useAuthContext } from "../context/AuthContext";
-
+import { useAuthContext } from "../context/AuthContext";
 
 const Sidebar = () => {
-	const authUser = true;
+	const { authUser } = useAuthContext();
+
 	return (
 		<aside
-			className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-glass'
+			className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8
+      overflow-y-auto border-r bg-glass'
 		>
 			<nav className='h-full flex flex-col gap-3'>
 				<Link to='/' className='flex justify-center'>
 					<img className='h-8' src='/github.svg' alt='Github Logo' />
 				</Link>
-                <Link
+
+				<Link
 					to='/'
 					className='p-1.5 flex justify-center transition-colors duration-200 rounded-lg 
 					hover:bg-gray-800'
@@ -27,7 +28,7 @@ const Sidebar = () => {
 					<IoHomeSharp size={20} />
 				</Link>
 
-                {authUser && (
+				{authUser && (
 					<Link
 						to='/likes'
 						className='p-1.5 flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800'
@@ -36,7 +37,7 @@ const Sidebar = () => {
 					</Link>
 				)}
 
-                {authUser && (
+				{authUser && (
 					<Link
 						to='/explore'
 						className='p-1.5 flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800'
@@ -45,7 +46,7 @@ const Sidebar = () => {
 					</Link>
 				)}
 
-                {!authUser && (
+				{!authUser && (
 					<Link
 						to='/login'
 						className='p-1.5 focus:outline-nones transition-colors duration-200 rounded-lg hover:bg-gray-800'
@@ -54,7 +55,7 @@ const Sidebar = () => {
 					</Link>
 				)}
 
-                {!authUser && (
+				{!authUser && (
 					<Link
 						to='/signup'
 						className='p-1.5 focus:outline-nones transition-colors duration-200 rounded-lg hover:bg-gray-800'
@@ -63,19 +64,13 @@ const Sidebar = () => {
 					</Link>
 				)}
 
-                {authUser && (
+				{authUser && (
 					<div className='flex flex-col gap-2 mt-auto'>
 						<Logout />
 					</div>
 				)}
-
-
-
-
-
 			</nav>
 		</aside>
 	);
 };
-
 export default Sidebar;
